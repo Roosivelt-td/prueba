b13c156def2204c4ab8bd408b43e46fd391e9956
roosivelt-td <roosivelt.td@gmail.com>
2025-06-17 20:21:33 -0500

    automovil_proyecto

---
+++ src/main/java/automoviles/controller/AutoController.java
@@ -0,0 +1,43 @@
+package automoviles.controller;
+
+import automoviles.dto.request.AutoRequest;
+import automoviles.dto.response.AutoResponse;
+import automoviles.service.AutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/autos")
+public class AutoController {
+
+    @Autowired
+    private AutoService autoService;
+
+    @PostMapping ("/create")// crear auto
+    public void crearAuto(@RequestBody AutoRequest request) { autoService.crearAuto(request);}
+
+    @GetMapping("/{id}") // obtener un auto por su id
+    public ResponseEntity<AutoResponse> obtenerAutoPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(autoService.obtenerAutoPorId(id));
+    }
+    @GetMapping ("/todos")// obtener todos los autos
+    public ResponseEntity<Collection<AutoResponse>> obtenerTodosLosAutos() {
+        return ResponseEntity.ok(autoService.obtenerTodosLosAutos());
+    }
+    @PutMapping("/update/{id}")// actualizar un auto por id
+    public void actualizarAutoId(@PathVariable Long id, @RequestBody AutoRequest request) {
+        if (autoService.obtenerAutoPorId(id) != null) {
+            autoService.actualizarAuto(id, request);
+        } else {
+            throw new RuntimeException("No existe un auto con el id: " + id);
+        }
+    }
+    @DeleteMapping("/delete/{id}") // eliminar un auto  por id
+    public void eliminarAutoId(@PathVariable Long id) {
+        autoService.eliminarAuto(id);
+    }
+
+}
---
+++ src/main/java/automoviles/service/AutoService.java
@@ -0,0 +1,15 @@
+package automoviles.service;
+
+import automoviles.dto.request.AutoRequest;
+import automoviles.dto.response.AutoResponse;
+
+import java.util.Collection;
+
+public interface AutoService {
+
+    Collection<AutoResponse> obtenerTodosLosAutos();
+    AutoResponse obtenerAutoPorId(Long id);
+    void crearAuto(AutoRequest request);
+    void actualizarAuto(Long id, AutoRequest request);
+    void eliminarAuto(Long id);
+}
